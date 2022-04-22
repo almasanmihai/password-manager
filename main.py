63a27a4da900470fd0daf77762dcf7442cf97baf
almasanmihai <almasanmihai@yahoo.com>
2022-04-22 12:54:53 +0300

    Done (with Angela requierements)

--- main.py
+++ main.py
@@ -1,15 +1,43 @@
 from tkinter import *
+from tkinter import messagebox
+import random
+import pyperclip
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+               'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+               'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    nr_letters = random.randint(8, 10)
+    nr_symbols = random.randint(2, 4)
+    nr_numbers = random.randint(2, 4)
+
+    pas_char = [random.choice(letters) for _ in range(nr_letters)]
+    pas_num = [random.choice(numbers) for _ in range(nr_numbers)]
+    pas_symbol = [random.choice(symbols) for _ in range(nr_symbols)]
+    password_list = pas_char + pas_symbol + pas_num
+    random.shuffle(password_list)
+    password = "".join(password_list)
+    pas_entry.delete(0, END)
+    pas_entry.insert(0, password)
+    pyperclip.copy(password)
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save():
-    with open("data.txt", "a") as file:
-        file.write(f"{web_entry.get()} | {mail_entry.get()} | {pas_entry.get()}\n")
-    web_entry.delete(0, END)
-    mail_entry.delete(0, END)
-    pas_entry.delete(0, END)
-    mail_entry.insert(0, "someone@example.com")
-
+    if len(web_entry.get()) == 0 or len(pas_entry.get()) == 0:
+        messagebox.showinfo(title="Oops", message="Please don't leave any fields empty")
+    else:
+        is_ok = messagebox.askokcancel(title=f"{web_entry}",
+                                       message=f"These are details entered:\nEmail: {mail_entry.get()}\nPassword: {pas_entry.get()} \nIs it ok to save?")
+        if is_ok:
+            with open("data.txt", "a") as file:
+                file.write(f"{web_entry.get()} | {mail_entry.get()} | {pas_entry.get()}\n")
+            web_entry.delete(0, END)
+            mail_entry.delete(0, END)
+            pas_entry.delete(0, END)
+            mail_entry.insert(0, "someone@example.com")
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -42,7 +70,7 @@ mail_entry.insert(0, "someone@example.com")
 pas_entry = Entry()
 pas_entry.grid(column=1, row=3, sticky="EW")
 
-gen_pas = Button(text="Generate Password")
+gen_pas = Button(text="Generate Password", command=generate_password)
 gen_pas.grid(column=2, row=3, sticky="EW")
 
 add_pas = Button(text="Add", command=save)
